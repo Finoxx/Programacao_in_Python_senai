1655e4ef2945f6f003485a9604527156ac0d6701
Henrique Bomfim <125929669+Finoxx@users.noreply.github.com>
2025-10-11 15:13:44 -0300

    Add files via upload

---
+++ aula4/aula5.py
@@ -0,0 +1,55 @@
+# print('atividade1')
+# print('Boas Vindas')
+
+print('atividade2')
+idade = 22 > 18
+print(idade)
+print(type(idade))
+
+# print('atividade3')
+# n1 = 25.5 
+# n2 = 22.7
+# calc = n1 - n2
+# print(calc)
+
+
+# print('atividade4')
+# n1 = 27 
+# n2 = 5
+# calc = n1 / n2
+# print(calc)
+
+# print('atividade5')
+# n1 = 26 
+# n2 = 5
+# calc = n1 - n2
+# print(calc)
+
+# print('atividade6')
+# n1 = 25
+# n2 = 22
+# calc = n1 // n2
+# print(calc)
+
+# print('atividade7')
+# n1 = 25.5 
+# n2 = 22.7
+# n3 = 23.4
+# n4 = 21.1
+# calc = n1 * n2 * n3 * n4
+# print(calc)
+
+# print('atividade8')
+# n1 = 25 
+# print(n1 *2)
+
+# print('atividade9')
+
+# nome = str(input('Digite seu nome completo: '))
+# idade = int(input('Digite a sua idade: '))
+# Cf = int(input('Digite os numeros do seu cartão: '))
+# Cv = int(input('Digite a data de vencimento do seu cartão: '))
+# Cvv = int(input('Digite o CVV do cartão: '))
+# Cb = str(input('Qual é a bandeira do seu cartão? '))
+
+# print('Seu nome é', nome , 'Sua idade é', idade , 'O numero do cartão é : ', Cf, 'A data de vencimento é: ', Cv , 'O CVV do cartão é: ', Cvv , 'A bandeira do cartão é: ', Cb)
