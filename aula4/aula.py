88a7603b77a128e5dc48d2b1a170242258777e86
Henrique Bomfim <125929669+Finoxx@users.noreply.github.com>
2025-10-11 12:09:30 -0300

    Add files via upload

---
+++ aula4/aula.py
@@ -0,0 +1,121 @@
+# # sinais ariméticos 
+# # +    -    *   /   //    %    **0.5
+# # Saida é sempre um número
+
+# n1  = 10
+# n2  = 20
+
+# print(n1  +  n2 )
+# print(n1  -  n2 )
+# print(n1  *  n2 )
+# print(n1  /  n2 )
+
+# # primeiro valor decimal  
+# print(n1  //  n2 )
+
+# # resto da conta de divisão 
+# print(10  %   2 )
+
+# # sinais lógicos -  saída é sempre True ou False
+# n1  = 10
+# n2  = 2
+# print(n1 > n2)# maior
+# print(n1 < n2)# menor  
+# print(n1 >= n2)# maior ou igual
+# print(n1 <= n2)# menor ou igual
+# print(n1 != n2)# diferente
+# print(n1 == n2)# igual
+
+
+# print('objeto', 10, n1) # output -- saída
+
+# nome = input('digite seu nome: ') # input -- entrada
+
+# # o Input naturalmente gera um texto 
+
+# n1  =  float(input('Digite um número: '))
+# n2  =  float(input('Digite outro número: '))
+# print(n1 + n2)
+
+# n1  =  float(input('Digite um número: '))
+# n2  =  float(input('Digite outro número: '))
+# print(n1 - n2)
+
+# n1  =  float(input('Digite um número: '))
+# n2  =  float(input('Digite outro número: '))
+# print(n1 / n2)
+
+# n1  =  float(input('Digite um número: '))
+# n2  =  float(input('Digite outro número: '))
+# print(n1 * n2)
+
+
+# concatenação -- juntar dados
+
+# nome  =  input('Digite seu nome: ')
+# idade = int(input('Digite sua idade: '))
+# endereco = input('Digite seu endereço: ')
+# curso = input('Curso: ')
+# salario =  float(input('Digite seu salário: '))
+# # concatenar 
+# print('Nome', nome, 'idade: ',idade)
+# print(f'Nome {nome} idade {idade}')
+# print('Nome'+' '+nome+' '+ 'idade'+ ' ', idade)
+# print('Nome {} idade {}'.format(nome, idade))
+# print('Nome %s idade %d'%(nome, idade))
+
+
+
+# pular linha
+# print()
+# f''' '''
+# \n
+
+# f'''  sdfsdfhsdlfsdf  {}''' cerquilha 
+# print(f'''
+# Nome {nome}
+# Idade {idade}
+# Endereço {endereco}
+# Curso {curso}
+# Salario {salario}          
+# ''')
+# # \n
+# print('Nome\n', nome, 'idade\n',idade)
+# #print
+
+# print('Sua Idade é:', idade)
+# print('Endereço: ', endereco)
+# print('Curso', curso)
+# print('Salário R$', salario)
+
+print("Atividade1: ")
+
+n1 = int(input("digite um número inteiro: "))
+calc = n1* n1
+print(calc)
+
+
+# print('Atividade2: ')
+
+# nome = input('digite o seu nome: ')
+# sobrenome = input('Digite o seu sobrenome:')
+# print('Nome\n', nome,  '\nSobrenome\n', sobrenome)
+
+# print('Atividade3: ')
+
+# n1 = input('Digite um numero')
+# n2 = input('Digite outro numero')
+#print(str(n1),str(n2))
+
+# print('Atividade4: ')
+
+# nome = "python"
+# n3 = int(input('digite um número: '))
+# print (nome, n3)
+
+# print('Atividade5: ')
+
+# frase1 = "Você pensa que o flamengo é time?"
+# print(frase1)
+# frase2 = str(input('Qual é a sua resposa?'))
+# print(frase1, frase2, 'O FLAMENGO E SELEÇÂO')
