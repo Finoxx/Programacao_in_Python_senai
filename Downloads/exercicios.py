5799e7330f1a39d3450334997c1ed26350aebbd9
Henrique Bomfim <125929669+Finoxx@users.noreply.github.com>
2025-11-29 10:37:32 -0300

    Add files via upload

---
+++ Downloads/exercicios.py
@@ -0,0 +1,37 @@
+# print ('Exercicio1')
+
+import os
+import shutil
+
+# with open('novo_diretorio', 'w') as novo_arquivo:
+#     os.mkdir('novo_arquivo')
+
+# with open('exemplo.txt', 'r') as arquivo:
+#     conteudo = arquivo.read()
+#     print (conteudo)
+
+# print ('Exercicio2')
+
+
+# with open('novo_diretorio', 'w') as novo_arquivo:
+#     os.mkdir('novo_arquivo')
+
+# print ('Exercicio3')
+
+
+# os.rename('senai', 'senai1')
+
+# print ('Exercicio4')
+# with os.scandir('C:/Users/Aluno/Downloads/') as entr:
+#     for dados in entr:
+#         print (dados.name)
+
+
+print ('Exercicio5')
+
+shutil.copytree('u', 'anoesbombados.txt')
+
+
+# print ('exercicio6')
+
+# shutil.rmtree('c:/Users/aluno/Downloads/novo_arquivo/')
